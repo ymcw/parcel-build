b426e1a59343afdf743e1f27aec2047b7de8d5da
ymcw <ymcw.tech@gmail.com>
2018-06-13 17:56:37 +0900

    base

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import $ from 'jquery';
+
+// import style
+import '../scss/app.scss';
+
+export default class App {
+  
+  constructor() {
+    this.setup();
+  }
+  
+  setup() {
+  }
+  
+}
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import App from './App';
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  new App();
+});
